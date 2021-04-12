ef3202007b120027cccb5c1d071a356323b6facd
Luiz Philipe Alves <luizphilipe02@gmail.com>
2021-04-11 22:41:19 -0300

    Lista de exercicio de arquivos

---
+++ PPGCC02 - Estrutura de Dados/6 - Arquivos/displayASCII.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main() {  
+    char c;
+    printf("Enter a character: ");
+    scanf("%c", &c);  
+    
+    // %d displays the integer value of a character
+    // %c displays the actual character
+    printf("ASCII value of %c = %d", c, c);
+    
+    return 0;
+}
